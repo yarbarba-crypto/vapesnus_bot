086ad9883fcb6dc776f81b770d12a67ea2bc6f0a
yarbarba-crypto <yar.barba@gmail.com>
2026-05-14 23:19:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,11 +4,13 @@ from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import BOT_TOKEN
+from database import init_db
 from handlers import catalog, orders, admin, start, support
 
 logging.basicConfig(level=logging.INFO)
 
 async def main():
+    init_db()
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher(storage=MemoryStorage())
 
