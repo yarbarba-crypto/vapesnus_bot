4bd5dc38f94a95813cbe6a38496c5d9557f28be9
yarbarba-crypto <yar.barba@gmail.com>
2026-05-15 19:40:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-BOT_TOKEN = "8688340215:AAHaKUjKfwByFr9IPBj0h28VPjObN40v1b4"
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 ADMIN_IDS = [634331402]
-ADMIN_PASSWORD = "cosmos7.2"  # Замени на свой пароль
-MAX_REQUESTS_PER_MINUTE = 10  # Максимум запросов в минуту
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "cosmos7.2")
+MAX_REQUESTS_PER_MINUTE = 10
