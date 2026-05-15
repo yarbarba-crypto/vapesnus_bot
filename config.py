fd495a8feafd78719fdd041c4fc81d79472e5dee
yarbarba-crypto <yar.barba@gmail.com>
2026-05-15 19:33:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 BOT_TOKEN = "8688340215:AAHaKUjKfwByFr9IPBj0h28VPjObN40v1b4"
 ADMIN_IDS = [634331402]
+ADMIN_PASSWORD = "cosmos7.2"  # Замени на свой пароль
+MAX_REQUESTS_PER_MINUTE = 10  # Максимум запросов в минуту
